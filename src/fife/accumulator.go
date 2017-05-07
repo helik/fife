33afce02b7475a6e25f90f9831daba5b2b8abb78
gailin <glnpease@dhcp-18-111-23-186.dyn.mit.edu>
2017-05-07 15:51:34 -0400

    Reorganizing and adding to web crawler code

--- src/fife/accumulator.go
+++ src/fife/accumulator.go
@@ -12,3 +12,26 @@ var FSumAccumulator = Accumulator {
         return originalValue.(float64) + newValue.(float64)
     },
 }
+
+//Just returns the first value, for when we don't care which of simultaneous updates
+//is returned.
+var FirstAccumulator = Accumulator {
+    Init: func(value interface{}) interface{} { return value },
+    Accumulate: func(originalValue interface{}, newValue interface{}) interface{} {
+        return originalValue
+    },
+}
+
+//Create an accumulator that returns the max of the inputs
+//compare returns true if first > second
+func CreateMaxAccumulator(compare func(interface{}, interface{}) bool) Accumulator {
+  return Accumulator{
+    Init: func(value interface{}) interface{} {return value},
+    Accumulate: func (initialValue interface{}, newValue interface{}) interface{} {
+      if compare(initialValue, newValue) {
+        return initialValue
+      }
+      return newValue
+    },
+  }
+}
